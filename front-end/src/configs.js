35deabb493c81afc7aeb404491f4111a388b18ea
Gjergj Kadriu <gjergjk71@gmail.com>
2021-02-27 22:30:58 +0100

    PUSH

--- front-end/src/configs.js
+++ front-end/src/configs.js
@@ -1,2 +1,11 @@
 
+let hostname = window.location.host.split(":")[0]
+
+let baseURL;
+
+if (hostname === "localhost") {
+    baseURL = "http://localhost:4848/api/v1"
+} else {
+    baseURL = "http://3.15.153.64:4000/"
+}
 export const baseURL = "http://localhost:4848/api/v1"
